a2bcb94c32620d7109951881038bb8d081dd3c03
jean salvi <38810299+salviosage@users.noreply.github.com>
2020-12-21 14:13:50 +0200

    initialize whole app (#2)

---
+++ src/constants/error.constants.ts
@@ -0,0 +1,8 @@
+
+export class ErrorConstants {
+    public static readonly USER_NOT_AUTHORIZED = 'You are not Authorized to access this API !!';
+    public static readonly USER_NOT_VERIFIED = 'Your account is not verified.please verify account or contact admin !!';
+  };
+  
+  
+  
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -2,6 +2,7 @@ import 'graphql-import-node';
 import * as rootDefs from './schemas/schema.graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from './resolvers/resolvers';
+import {customAuthChecker} from '../decorators/auth.decorator'
 
 const schema = makeExecutableSchema({
   typeDefs: [rootDefs],
